0ee3ad0b4a9fdd4d5291d26c83080401fdbdbcac
Anirudh Kamath <github@kamath.io>
2024-11-06 21:40:14 -0800

    iamges

--- components/client/page.tsx
+++ components/client/page.tsx
@@ -8,6 +8,15 @@ import { useEffect, useRef, useState } from "react";
 import { getTopTracks } from "@/lib/spotify";
 import { getTopArtists } from "@/lib/spotify";
 
+const food = [
+  "01_malai_kofta.JPG",
+  "02_rasgulla.jpg",
+  "03_salmon_tartare.jpg",
+  "04_pasta.jpg",
+  "05_tg.jpg",
+  "06_rava_dosa.jpg",
+];
+
 export default function Page() {
   const [topTracks, setTopTracks] = useState<Track[]>([]);
   const [topArtists, setTopArtists] = useState<Artist[]>([]);
@@ -49,29 +58,47 @@ export default function Page() {
             </MotionDiv>
           )}
         </div>
-        <div className="grid grid-cols-2 overflow-hidden w-full">
-          {[
-            "01_malai_kofta.JPG",
-            "02_rasgulla.jpg",
-            "03_salmon_tartare.jpg",
-            "04_pasta.jpg",
-            "05_tg.jpg",
-            "06_rava_dosa.jpg",
-          ].map((item: string, index: number) => (
-            <div
-              className="flex flex-col items-center gap-4 min-w-[200px]"
-              key={index}
-            >
-              <div className="relative flex items-center justify-center aspect-square w-full bg-gray-200  overflow-hidden">
-                <Image
-                  src={`/img/food/${item}`}
-                  alt="food"
-                  fill
-                  className=" object-cover"
-                />
-              </div>
+        <div className="p-4 flex items-center justify-center w-full">
+          <div className="flex justify-center overflow-hidden w-full gap-4">
+            <div className="flex flex-col gap-4">
+              {food
+                .filter((_, index) => index % 2 === 0)
+                .map((item: string, index: number) => (
+                  <div
+                    className="flex flex-col items-center gap-4 min-w-[150px]"
+                    key={index}
+                  >
+                    <div className="relative flex items-center justify-center aspect-square w-full bg-gray-200  overflow-hidden">
+                      <Image
+                        src={`/img/food/${item}`}
+                        alt="food"
+                        fill
+                        className="rounded-lg object-cover"
+                      />
+                    </div>
+                  </div>
+                ))}
             </div>
-          ))}
+            <div className="flex flex-col gap-4 mt-6">
+              {food
+                .filter((_, index) => index % 2 === 1)
+                .map((item: string, index: number) => (
+                  <div
+                    className="flex flex-col items-center gap-4 min-w-[150px]"
+                    key={index}
+                  >
+                    <div className="relative flex items-center justify-center aspect-square w-full bg-gray-200  overflow-hidden">
+                      <Image
+                        src={`/img/food/${item}`}
+                        alt="food"
+                        fill
+                        className="rounded-lg object-cover"
+                      />
+                    </div>
+                  </div>
+                ))}
+            </div>
+          </div>
         </div>
       </div>
     </div>
